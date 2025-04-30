b139f787b0c06b546fb7a29fc99811c4c037b10b
Yash Jain <140283602+yashj1579@users.noreply.github.com>
2025-04-30 09:46:31 -0500

    Update test_worker.py based on integration feedback

--- test/test_worker.py
+++ test/test_worker.py
@@ -3,10 +3,10 @@ import time
 import requests
 
 def test_do_work():
-    job_dict = jobs.add_job(2000,3000)
+    job_dict = jobs.add_job(2000,3000, "1745952031")
     id = job_dict["id"]
     time.sleep(10)  #wait at least 5 seconds to allow worker to complete task
-    response = requests.get(f"http://127.0.0.1:5001/results/{id}")
+    response = requests.get(f"http://127.0.0.1:5000/results/{id}")
     assert response.status_code == 200
     assert "message" not in response.json()
     assert "error" not in response.json()
