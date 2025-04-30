395b3423ebe62ecf3fbb6951ecf1990783e4381b
Yash Jain <140283602+yashj1579@users.noreply.github.com>
2025-04-30 09:46:13 -0500

    Update test_jobs.py based on integration feedback

--- test/test_jobs.py
+++ test/test_jobs.py
@@ -2,11 +2,11 @@ from jobs import add_job
 import pytest
 
 def test_add_job():
-    job = add_job(2000, 3000)
+    job = add_job(2000, 3000, "1745952031")
     assert isinstance(job, dict)
     assert job['min_altitude'] == 2000
     assert job['max_altitude'] == 3000
+    assert job['hour'] == "1745952031"
 
-def test_fail_add_job():
-    job = add_job(None, None)
+    job = add_job(None, None, None)
     assert job is False
