5532483a2c691fdd637bfd84dcdf309ac1501230
Yash Jain <140283602+yashj1579@users.noreply.github.com>
2025-04-29 19:30:52 -0500

    Create test_worker.py

---
+++ test/test_worker.py
@@ -0,0 +1,12 @@
+import jobs
+import time
+import requests
+
+def test_do_work():
+    job_dict = jobs.add_job(2000,3000)
+    id = job_dict["id"]
+    time.sleep(10)  #wait at least 5 seconds to allow worker to complete task
+    response = requests.get(f"http://127.0.0.1:5001/results/{id}")
+    assert response.status_code == 200
+    assert "message" not in response.json()
+    assert "error" not in response.json()
