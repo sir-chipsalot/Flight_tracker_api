6a63ab90f6636bdaa60473c84af8c624dd7d71c0
Yash Jain <140283602+yashj1579@users.noreply.github.com>
2025-04-30 20:07:22 -0500

    Update test_worker.py to work correctly with jobs.py

--- test/test_worker.py
+++ test/test_worker.py
@@ -1,12 +1,19 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
 import jobs
 import time
 import requests
 
+
 def test_do_work():
-    job_dict = jobs.add_job(2000,3000, "1745952031")
+    response = requests.get("http://127.0.0.1:5000/time")
+    sv = response.json()["times"][0]
+    job_dict = jobs.add_job(2000,3000, sv)
     id = job_dict["id"]
     time.sleep(10)  #wait at least 5 seconds to allow worker to complete task
-    response = requests.get(f"http://127.0.0.1:5000/results/{id}")
+    response = requests.get(f"http://127.0.0.1:5000/results-dat/{id}")
     assert response.status_code == 200
     assert "message" not in response.json()
     assert "error" not in response.json()
