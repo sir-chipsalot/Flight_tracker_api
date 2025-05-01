c1726c566da018291ab3b826b2aaa7a305f67dca
Yash Jain <140283602+yashj1579@users.noreply.github.com>
2025-04-30 20:06:52 -0500

    Update test_jobs.py to correctly call add_job function

--- test/test_jobs.py
+++ test/test_jobs.py
@@ -1,12 +1,21 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
+
 from jobs import add_job
 import pytest
+import requests
 
 def test_add_job():
-    job = add_job(2000, 3000, "1745952031")
+    response = requests.get("http://127.0.0.1:5000/time")
+    sv = response.json()["times"][0]
+    job = add_job(2000, 3000, sv)
     assert isinstance(job, dict)
     assert job['min_altitude'] == 2000
     assert job['max_altitude'] == 3000
-    assert job['hour'] == "1745952031"
+    assert job['hour'] == sv
 
     job = add_job(None, None, None)
     assert job is False
