240ef3e6285ca49e3ec91d2ac3fdb8515a750cd8
Yash Jain <140283602+yashj1579@users.noreply.github.com>
2025-04-30 20:06:31 -0500

    Update test_api.py to function as expected

--- test/test_api.py
+++ test/test_api.py
@@ -3,7 +3,9 @@ import json
 import pytest
 
 def test_count_flights():
-    response = requests.get("http://127.0.0.1:5000/planes/count_flights/1745952031")
+    response = requests.get("http://127.0.0.1:5000/time")
+    sv = response.json()["times"][0]
+    response = requests.get(f"http://127.0.0.1:5000/count_flights/{sv}")
     assert response.status_code == 200
 
     data = response.json()
@@ -18,28 +20,36 @@ def test_count_flights():
 
 def test_create_jobs():
     job = {"min_altitude": 10000, "max_altitude": 40000}
-    response = requests.post(f"http://127.0.0.1:5000/jobs/1745952031", json=job)
+    response = requests.get("http://127.0.0.1:5000/time")
+    sv = response.json()["times"][0]
+    response = requests.post(f"http://127.0.0.1:5000/jobs/{sv}", json=job)
     assert response.status_code == 200
     assert response.json()["message"] == "job successfully created"
 
     job = {"min_altitude": 10000}
-    response = requests.post(f"http://127.0.0.1:5000/jobs/1745952031", json=job)
+    response = requests.post(f"http://127.0.0.1:5000/jobs/{sv}", json=job)
     assert response.status_code == 400
     assert "Missing required parameters" in response.json()["error"]
 
 def test_list_jobs():
     response = requests.get(f"http://127.0.0.1:5000/jobs")
     assert response.status_code == 200
-    assert isinstance(response.json(), list)
+    data = response.json()
+    assert isinstance(data, dict)
+    assert "job ids" in data
+    assert isinstance(data["job ids"], list)
 
 def test_get_job():
     response = requests.get(f"http://127.0.0.1:5000/jobs/fake_id")
     assert response.status_code == 404
     assert "job id not found" in response.json()["error"]
 
+    response = requests.get("http://127.0.0.1:5000/time")
+    sv = response.json()["times"][0]
     job = {"min_altitude": 10000, "max_altitude": 40000}
-    response = requests.post(f"http://127.0.0.1:5000/jobs", json=job)
-    response = requests.get(f"http://127.0.0.1:5000/jobs/{response.json()['id']}")
+    response = requests.post(f"http://127.0.0.1:5000/jobs/{sv}", json=job)
+    ids = response.json()['id']
+    response = requests.get(f"http://127.0.0.1:5000/jobs/{ids}")
     assert response.status_code == 200
 
 def test_get_results_dat():
@@ -47,9 +57,12 @@ def test_get_results_dat():
     assert response.status_code == 404
     assert "job id not found" in response.json()["error"]
 
+    response = requests.get("http://127.0.0.1:5000/time")
+    sv = response.json()["times"][0]
     job = {"min_altitude": 10000, "max_altitude": 40000}
-    response = requests.post(f"http://127.0.0.1:5000/jobs", json=job)
-    response = requests.get(f"http://127.0.0.1:5000/results-dat/{response.json()['id']}")
+    response = requests.post(f"http://127.0.0.1:5000/jobs/{sv}", json=job)
+    ids = response.json()['id']
+    response = requests.get(f"http://127.0.0.1:5000/results-dat/{ids}")
     assert response.status_code == 200
 
 def test_get_results_img():
@@ -57,7 +70,10 @@ def test_get_results_img():
     assert response.status_code == 404
     assert "job id not found" in response.json()["error"]
 
+    response = requests.get("http://127.0.0.1:5000/time")
+    sv = response.json()["times"][0]
     job = {"min_altitude": 10000, "max_altitude": 40000}
-    response = requests.post(f"http://127.0.0.1:5000/jobs", json=job)
-    response = requests.get(f"http://127.0.0.1:5000/results-img/{response.json()['id']}")
+    response = requests.post(f"http://127.0.0.1:5000/jobs/{sv}", json=job)
+    ids = response.json()['id']
+    response = requests.get(f"http://127.0.0.1:5000/results-img/{ids}")
     assert response.status_code == 200
