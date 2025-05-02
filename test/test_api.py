521a5bd4c8845d265d5be67cc3d34592571c9e88
Yash Jain <140283602+yashj1579@users.noreply.github.com>
2025-05-02 16:15:04 -0500

    Update test_api.py

--- test/test_api.py
+++ test/test_api.py
@@ -78,8 +78,4 @@ def test_get_results_img():
     response = requests.get(f"http://127.0.0.1:5000/results-img/{ids}")
     assert response.status_code == 200
 
-def test_delete_data():
-    response = requests.delete("http://127.0.0.1:5000/data")
-    assert response.status_code == 200
-    #repopulate data in case it becomes empty
-    response = requests.post("http://127.0.0.1:5000/data")
+
